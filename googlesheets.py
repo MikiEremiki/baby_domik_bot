72ebe8f365cfbf67a842d95e549b964c9ff4f2dd
MikiEremiki <52360858+MikiEremiki@users.noreply.github.com>
2023-02-16 11:33:17 +0300

    Дописал функцию записи в Клиентскую базу

--- googlesheets.py
+++ googlesheets.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -81,44 +83,82 @@ def confirm(row, numbers):
         )
         response = request.execute()
 
-        pprint(response)
+        print(
+            'spreadsheetId: ',
+            response['spreadsheetId'],
+            '\n'
+            'updatedRange: ',
+            response['updatedRange']
+        )
 
     except HttpError as err:
         print(err)
 
 
-def write_client(client: dict):
+def write_client(
+        client: dict,
+        row_in_data_show: str,
+        dict_reserve_option: dict
+):
     try:
         values_column = get_values(
             SPREADSHEET_ID['Домик'],
             RANGE_NAME['База клиентов']
+        )
+        values_row = get_values(
+            SPREADSHEET_ID['Домик'],
+            RANGE_NAME['База спектаклей_'] + f'A{row_in_data_show}:C{row_in_data_show}'
         )
 
         if not values_column:
             print('No data found.')
             return
 
-        last_row = len(values_column) + 1
+        first_row_for_write = len(values_column)
+        last_row_for_write = first_row_for_write + len(client['data_children'])
+
         sheet = get_service_sacc(SCOPES).spreadsheets()
-        value_input_option = 'RAW'
-        values = [[]]
-        for item in client.items():
-            values[0].append(item[1])
+        value_input_option = 'USER_ENTERED'
+        response_value_render_option = 'FORMATTED_VALUE'
+        values = []
+
+        for i in range(len(client['data_children'])):
+            values.append([first_row_for_write + i])
+            for key, item in client.items():
+                if key == 'data_children':
+                    item = item[i]
+                    values[i].append(item[0])
+                    values[i].append(item[1])
+                else:
+                    values[i].append(item)
+            values[i].append(f'=(TODAY()-E{first_row_for_write + i})/365')
+            values[i].extend(values_row[0])
+            values[i].append(datetime.now().__str__())
+            for key, value in dict_reserve_option.items():
+                if key == 'flag_individual':
+                    break
+                values[i].append(value)
+
         print(values)
+        end_column_index = len(values[0])
+
         value_range_body = {
             'values': values,
         }
-        range_sheet = f'Клиентская база!B{last_row}:D{last_row}'
+
+        range_sheet = f'Клиентская база!R{first_row_for_write + 1}C1:' \
+                      f'R{last_row_for_write + 1}C{end_column_index}'
 
         request = sheet.values().update(
             spreadsheetId=SPREADSHEET_ID['Домик'],
             range=range_sheet,
             valueInputOption=value_input_option,
+            responseValueRenderOption=response_value_render_option,
             body=value_range_body
         )
         response = request.execute()
 
-        pprint(
+        print(
             'spreadsheetId: ',
             response['spreadsheetId'],
             '\n'
