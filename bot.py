ef1d5cac66e75556c27d6fa47e9803c3f595c039
MikiEremiki <mr.eremin.ma@ya.ru>
2023-07-12 22:17:48 +0300

    Review. Вынес def conv хэндлеров в отдельный файл

--- bot.py
+++ bot.py
@@ -2,13 +2,11 @@ from telegram.ext import (
     Application,
     CommandHandler,
     CallbackQueryHandler,
-    ConversationHandler,
-    MessageHandler,
-    filters,
     PicklePersistence
 )
 
 import handlers as hl
+from conv_hl.reserve_conv_hl import reserve_conv_hl
 from log_debug.logging_conf import load_log_config
 from utilites import echo, send_log, set_menu
 from settings import (
@@ -33,70 +31,8 @@ def bot():
     application.job_queue.run_once(set_menu, 0)
 
     application.add_handler(CommandHandler(COMMAND_DICT['START'][0], hl.start))
-    conv_handler = ConversationHandler(
-        entry_points=[
-            CommandHandler(COMMAND_DICT['RESERVE'][0], hl.choice_show),
-            CommandHandler(COMMAND_DICT['LIST'][0], hl.choice_show),
-        ],
-        states={
-            'DATE': [
-                CallbackQueryHandler(hl.cancel, pattern='^Отменить$'),
-                CallbackQueryHandler(hl.choice_time),
-            ],
-            'TIME': [
-                CallbackQueryHandler(hl.cancel, pattern='^Отменить$'),
-                CallbackQueryHandler(hl.back_date, pattern='^Назад$'),
-                CallbackQueryHandler(hl.choice_option_of_reserve),
-            ],
-            'ORDER': [
-                CallbackQueryHandler(hl.cancel, pattern='^Отменить$'),
-                CallbackQueryHandler(hl.back_time, pattern='^Назад$'),
-                CallbackQueryHandler(hl.check_and_send_buy_info),
-            ],
-            'PAID': [
-                CallbackQueryHandler(hl.cancel, pattern='^Отменить'),
-                MessageHandler(
-                    filters.PHOTO | filters.ATTACHMENT,
-                    hl.forward_photo_or_file
-                ),
-            ],
-            'FORMA': [
-                MessageHandler(filters.TEXT, hl.get_name_adult),
-            ],
-            'PHONE': [
-                MessageHandler(filters.TEXT, hl.get_phone),
-            ],
-            'CHILDREN': [
-                MessageHandler(filters.TEXT, hl.get_name_children),
-            ],
-            'LIST': [
-                CallbackQueryHandler(hl.cancel, pattern='^Отменить$'),
-                CallbackQueryHandler(hl.back_date, pattern='^Назад$'),
-                CallbackQueryHandler(hl.send_clients_data),
-            ],
-            'CHOOSING': [
-                MessageHandler(
-                    filters.Regex('^(Выбрать другое время)$'),
-                    hl.choice_show
-                ),
-                MessageHandler(
-                    filters.Regex('^(Записаться в лист ожидания)$'),
-                    hl.write_list_of_waiting
-                ),
-            ],
-            'PHONE_FOR_WAITING': [
-                MessageHandler(filters.TEXT, hl.get_phone_for_waiting),
-            ],
-            ConversationHandler.TIMEOUT: [hl.TIMEOUT_HANDLER]
-        },
-        fallbacks=[CommandHandler('help', hl.help_command)],
-        conversation_timeout=15*60,  # 15 мин
-        name="my_conversation",
-        persistent=True,
-        allow_reentry=True
-    )
 
-    application.add_handler(conv_handler)
+    application.add_handler(reserve_conv_hl)
     application.add_handler(CallbackQueryHandler(hl.confirm, pattern='^Разрешить'))
     application.add_handler(CallbackQueryHandler(hl.reject, pattern='^Отклонить'))
 
