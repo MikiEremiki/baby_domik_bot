9e18e964444df0b96810eabe19899f3e85943952
MikiEremiki <mr.eremin.ma@ya.ru>
2023-07-25 01:36:12 +0300

    Review. Добавление очистки контекста в случае долго ответа пользователя и срабатывании timeout handler'а

--- handlers/birthday_hl.py
+++ handlers/birthday_hl.py
@@ -585,6 +585,7 @@ async def conversation_timeout(
     ))
     birthday_hl_logger.info(f'Для пользователя {context.user_data["user"]}')
     birthday_hl_logger.info(f'Обработчик завершился на этапе {context.user_data["STATE"]}')
+    context.user_data.clear()
 
     return ConversationHandler.END
 
