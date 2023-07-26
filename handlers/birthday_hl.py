2343cce3c9625964f21e9b59040da3c75133e34d
MikiEremiki <mr.eremin.ma@ya.ru>
2023-07-27 01:31:29 +0300

    New feature.
    Запись данных заявки ДР в гугл-таблицу

--- handlers/birthday_hl.py
+++ handlers/birthday_hl.py
@@ -422,7 +422,7 @@ async def get_phone(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         context.user_data['message_id_for_admin'] = message.message_id
 
-        # TODO Создаем запись в гугл-таблице
+        write_client_bd(context.user_data['birthday_data'])
 
     except Exception as e:
         birthday_hl_logger.error(e)
