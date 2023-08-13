b67f8340d15f80cfc4ebd7254a91db12759ff188
MikiEremiki <mr.eremin.ma@ya.ru>
2023-08-13 17:51:56 +0300

    Refactoring
    Исправил неверный показ номера в подписях к кнопке, не соответствующий номеру спектакля в тексте сообщения

--- utilities/hlp_func.py
+++ utilities/hlp_func.py
@@ -44,7 +44,7 @@ def create_replay_markup_for_list_of_shows(
         match ver:
             case 1:
                 button_tmp = InlineKeyboardButton(
-                    text=key + ' ' + DICT_OF_EMOJI_FOR_BUTTON[num],
+                    text=key + ' ' + DICT_OF_EMOJI_FOR_BUTTON[item],
                     callback_data=str(item) + ' | ' + key
                 )
             case 2:
