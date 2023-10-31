20a825a97ae5a3050918004ce642e25318b99cdd
MikiEremiki <mr.eremin.ma@ya.ru>
2023-10-31 23:23:38 +0300

    feat: Добавить дополнительную пару kv в user_data в словарь с показами
    переход на использование event_id вместо номера строки

--- components/db/db_googlesheets.py
+++ components/db/db_googlesheets.py
@@ -74,10 +74,8 @@ def load_show_data() -> tuple[
     dict_of_name_show_flip = {}
     dict_of_date_show = {}
     j = 0
-    for i, item in enumerate(data):
-        i += first_row - 1
-        dict_of_shows[i + 1] = {
-            'event_id': item[dict_column_name['event_id']],
+    for item in data:
+        dict_of_shows[item[dict_column_name['event_id']]] = {
             'show_id': item[dict_column_name['show_id']],
             'name_show': item[dict_column_name['name_show']],
             'date_show': item[dict_column_name['date_show']],
