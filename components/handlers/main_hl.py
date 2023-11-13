7c613cb5c1db4d4e50cfc2ac92dd98c082fa2dbc
MikiEremiki <mr.eremin.ma@ya.ru>
2023-11-13 04:07:36 +0300

    fix: Переработать очистку контекста, чтобы удалялись данные явно и только нужные

--- components/handlers/main_hl.py
+++ components/handlers/main_hl.py
@@ -112,8 +112,6 @@ async def confirm_reserve(update: Update, context: ContextTypes.DEFAULT_TYPE):
             chat_id=chat_id,
         )
 
-        # TODO Решить нужна ли действительно очистка контекста пользователя
-        context.application.user_data.get(int(chat_id)).clear()
         # Сообщение уже было удалено самим пользователем
         try:
             await context.bot.delete_message(
@@ -174,7 +172,6 @@ async def reject_reserve(update: Update, context: ContextTypes.DEFAULT_TYPE):
                  'Татьяна Бурганова @Tanya_domik +79159383529',
             chat_id=chat_id,
         )
-        context.application.user_data.get(int(chat_id)).clear()
 
         # Сообщение уже было удалено самим пользователем
         try:
