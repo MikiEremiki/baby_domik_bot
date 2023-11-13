458730db9640aaac5d4550acaf4138ff826662cf
MikiEremiki <mr.eremin.ma@ya.ru>
2023-11-13 04:05:24 +0300

    feat: Добавить к callback_data сообщений payment_id

--- components/utilities/hlp_func.py
+++ components/utilities/hlp_func.py
@@ -171,20 +171,21 @@ def check_phone_number(phone):
 def create_approve_and_reject_replay(
         callback_name,
         chat_id,
-        message_id
+        message_id,
+        payment_id=None
 ):
     keyboard = []
 
     button_approve = InlineKeyboardButton(
         "Подтвердить",
         callback_data=f'confirm-{callback_name}|'
-                      f'{chat_id} {message_id}'
+                      f'{chat_id} {message_id} {payment_id}'
     )
 
     button_cancel = InlineKeyboardButton(
         "Отклонить",
         callback_data=f'reject-{callback_name}|'
-                      f'{chat_id} {message_id}'
+                      f'{chat_id} {message_id} {payment_id}'
     )
     keyboard.append([button_approve, button_cancel])
     return InlineKeyboardMarkup(keyboard)
