28f033841945b58fddf4be76f113b16daedf0925
MikiEremiki <mr.eremin.ma@ya.ru>
2023-12-08 09:17:35 +0300

    Add in 'dict_show_data' full dict about show to context_bot_data schema

--- components/utilities/schemas/context_bot_data.py
+++ components/utilities/schemas/context_bot_data.py
@@ -6,7 +6,23 @@ from ticket import list_of_tickets
 
 context_bot_data: Dict = {
     'list_of_tickets': list_of_tickets,
-    'dict_show_data': Dict,
+    'dict_show_data': {
+        'show_id': {  # int
+                'name': str,
+                'flag_premiere': bool,
+                'min_age_child': int,
+                'max_age_child': int,
+                'emoji': str,
+                'birthday': {
+                    'flag': bool,
+                    'max_num_child': int,
+                    'max_num_adult': int,
+                },
+                'flag_repertoire': bool,
+                'flag_indiv_cost': bool,
+                'full_name': str,
+        },
+    },
     'afisha': {
         int: str,  # int - номер месяца, str - file_id картинки
     },
