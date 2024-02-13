8be8875945e7871a0aff15f4ba00fe30c74d8e5d
MikiEremiki <mr.eremin.ma@ya.ru>
2024-02-13 23:28:17 +0300

    feat: Добавить функцию load_show_info в db_googlesheets
    Новая функция load_show_info получает информацию о событии из Google
    Sheets на основе предоставленного event_id
    
    Она проходит через данные
    таблицы и возвращает соответствующую запись о событии и идентификаторы
    столбцов

--- src/db/db_googlesheets.py
+++ src/db/db_googlesheets.py
@@ -336,3 +336,22 @@ def load_clients_wait_data(
             data_clients_data.append(item)
 
     return data_clients_data, dict_column_name
+
+
+def load_show_info(
+        event_id: int
+):
+    first_colum = get_data_from_spreadsheet(
+        RANGE_NAME['База спектаклей']
+    )
+    dict_column_name, len_column = get_column_info('База спектаклей_')
+    sheet = (
+            RANGE_NAME['База спектаклей_'] +
+            f'R1C1:R{len(first_colum)}C{len_column}'
+    )
+
+    data = get_data_from_spreadsheet(sheet)
+
+    for item in data[1:]:
+        if item[dict_column_name['event_id']] == str(event_id):
+            return item, dict_column_name
