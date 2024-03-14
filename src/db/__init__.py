a6b5210551c90cc07e002a981398d6433c94f8c9
MikiEremiki <mr.eremin.ma@ya.ru>
2024-03-14 22:16:57 +0300

    refactor: Добавление новых типов в __init__.py

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,6 +1,7 @@
 from .base import BaseModel, BaseModelTimed
-from .database import middleware_db_add_handlers
-from .enum_types import TicketStatusEnum, TicketPriceTypeEnum, PriceTypeEnum
-from .models import User, Child, Ticket, TypeEvent, TheaterEvent, ScheduleEvent
+from .database import middleware_db_add_handlers, create_sessionmaker_and_engine
+from .enum import (TicketStatusEnum, TicketPriceTypeEnum, PriceTypeEnum,
+                   AgeTypeEnum)
+from .models import (User, Person, Child, Ticket, TypeEvent, TheaterEvent,
+                     ScheduleEvent, Adult)
 from .pickle_persistence import pickle_persistence
-from .db_postgres import AsyncORM
