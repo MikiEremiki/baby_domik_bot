90ff42a4235fff78ede97aafdd51128adc9bf361
MikiEremiki <mr.eremin.ma@ya.ru>
2024-04-01 23:36:16 +0300

    refactor: Восстановление BaseTicket pydantic класса для обратной совместимости
    В pickle persistence хранятся объекты python, которые не сериализованы

--- src/utilities/schemas/ticket.py
+++ src/utilities/schemas/ticket.py
@@ -12,6 +12,8 @@ def clean_float(s: str) -> str:
 
 
 def str_to_date(s: str) -> (datetime | None):
+    if not s:
+        return None
     date_now = datetime.now().date()
     s = s.split()
     if len(s):
@@ -25,29 +27,99 @@ prepare_float = Annotated[float, BeforeValidator(clean_float)]
 prepare_str_to_date = Annotated[datetime | None, BeforeValidator(str_to_date)]
 
 
-class BaseTicketDTO(BaseModel):
+class BaseTicket(BaseModel):
     base_ticket_id: int  # Идентификатор билета
     flag_active: bool = True
     name: str  # Наименование вариантов билетов
-    # cost_basic: int  # Базовая стоимость
     cost_main: int  # Стоимость
-    cost_privilege: int  # Стоимость для льгот
     period_start_change_price: prepare_str_to_date = None
     # Начало периода изм цены
     period_end_change_price: prepare_str_to_date = None
     # Конец периода изм цены
     cost_main_in_period: int = None  # Стоимость на время повышения
-    cost_privilege_in_period: int  # Стоимость льгот на время повышения
-    # cost_basic_in_period: int  # Базовая стоимость на время повышения
     quality_of_children: int = 0  # Кол-во детск мест
-    # price_child_for_one_ticket: int  # Сумма за 1 билет
     quality_of_adult: int = 0  # Кол-во взр мест
-    # price_adult_for_one_ticket: int  # Сумма за 1 билет
     quality_of_add_adult: int = 0  # Кол-во доп взр мест
-    # flag_individual: bool = False  # Флаг для индивидуального обращения
-    # flag_season_ticket: bool  # Флаг абонемент
-    quality_visits: int  # Общее кол-во посещений по билету
-    # ticket_category: int  # Категория билета
+    flag_individual: bool = False  # Флаг для индивидуального обращения
+    ticket_category: int  # Категория билета
+
+    date_show_tmp: datetime = Field(
+        default=datetime.fromisoformat('2000-01-01'))
+
+    @computed_field
+    @property
+    def date_show(self) -> datetime:
+        return datetime.fromisoformat('2000-01-01')
+
+    @date_show.setter
+    def date_show(self, new_date: datetime) -> None:
+        self.date_show_tmp = new_date
+
+    @computed_field
+    @property
+    def flag_set_period_price(self) -> bool:
+        if self.period_start_change_price:
+            if self.period_start_change_price < self.date_show_tmp:
+                if self.period_end_change_price:
+                    if self.date_show_tmp < self.period_end_change_price:
+                        return True
+                    else:
+                        return False
+                else:
+                    return True
+            else:
+                return False
+        else:
+            return False
+
+    @computed_field
+    @property
+    def price(self) -> int:
+        if self.flag_set_period_price:
+            return self.cost_main_in_period
+        else:
+            return self.cost_main
+
+    @price.setter
+    def price(self, new_cost) -> None:
+        self.cost_main = new_cost
+
+    def __repr__(self):
+        return str(self.base_ticket_id)
+
+    def to_dto(self):
+        return {
+            'flag_active': self.flag_active,
+            'base_ticket_id': self.base_ticket_id,
+            'name': self.name,
+            'cost_main': self.cost_main,
+            'period_start_change_price': self.period_start_change_price,
+            'period_end_change_price': self.period_end_change_price,
+            'cost_main_in_period': self.cost_main_in_period,
+            'quality_of_children': self.quality_of_children,
+            'quality_of_adult': self.quality_of_adult,
+            'quality_of_add_adult': self.quality_of_add_adult,
+            'flag_individual': self.flag_individual,
+        }
+
+
+class BaseTicketDTO(BaseModel):
+    base_ticket_id: int  # Идентификатор билета
+    flag_active: bool = True
+    name: str  # Наименование вариантов билетов
+    cost_main: int  # Стоимость
+    cost_privilege: int = 0  # Стоимость для льгот
+    period_start_change_price: prepare_str_to_date = None
+    # Начало периода изм цены
+    period_end_change_price: prepare_str_to_date = None
+    # Конец периода изм цены
+    cost_main_in_period: int = None  # Стоимость на время повышения
+    cost_privilege_in_period: int = 0  # Стоимость льгот на время повышения
+    quality_of_children: int = 0  # Кол-во детск мест
+    quality_of_adult: int = 0  # Кол-во взр мест
+    quality_of_add_adult: int = 0  # Кол-во доп взр мест
+    quality_visits: int = 0  # Общее кол-во посещений по билету
+    flag_individual: bool = False  # Флаг для индивидуального обращения
 
     date_show_tmp: datetime = Field(
         default=datetime.fromisoformat('2000-01-01'))
@@ -108,6 +180,7 @@ class BaseTicketDTO(BaseModel):
             'quality_of_adult': self.quality_of_adult,
             'quality_of_add_adult': self.quality_of_add_adult,
             'quality_visits': self.quality_visits,
+            'flag_individual': self.flag_individual,
         }
 
 
