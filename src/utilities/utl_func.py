7f6273ded52737894b94fa58ff772df898bcb935
MikiEremiki <mr.eremin.ma@ya.ru>
2024-04-04 00:40:14 +0300

    fix: Перевод текста email в нижний регистр перед анализом регулярным выражением

--- src/utilities/utl_func.py
+++ src/utilities/utl_func.py
@@ -268,14 +268,14 @@ def extract_phone_number_from_text(phone):
     return re.sub(r'^[78]{,2}(?=9)', '', phone)
 
 
-def check_email(email):
+def check_email(email: str):
         return re.fullmatch(r"^[-a-z0-9!#$%&'*+/=?^_`{|}~]+"
                             r"(?:\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*"
                             r"@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*"
                             r"(?:aero|arpa|asia|biz|cat|com|coop|"
                             r"edu|gov|info|int|jobs|mil|mobi|museum|"
                             r"name|net|org|pro|tel|travel|[a-z][a-z])$",
-                            email)
+                            email.lower())
 
 
 def yrange(n):
