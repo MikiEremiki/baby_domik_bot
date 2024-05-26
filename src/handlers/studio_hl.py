2734b1da73b1f5d55170545141e7c5faa52bddec
MikiEremiki <mr.eremin.ma@ya.ru>
2024-05-27 00:16:55 +0300

    feat: Добавить обработчик studio_hl
    Был добавлен обработчик studio_hl.py, который включает в себя функции для обработки выбора мероприятия, даты и времени. Этот обработчик включает в себя логирование действий пользователя и обращение к базе данных Postgres.

---
+++ src/handlers/studio_hl.py
@@ -0,0 +1,94 @@
+import logging
+
+from telegram import Update
+from telegram.ext import ContextTypes, ConversationHandler, TypeHandler
+
+from db import db_postgres
+from handlers.sub_hl import get_theater_and_schedule_events_by_month
+from utilities.utl_func import set_back_context, create_event_names_text
+from utilities.utl_kbd import create_kbd_schedule_and_date, create_replay_markup
+
+studio_hl_logger = logging.getLogger('bot.studio_hl')
+
+
+async def choice_show_and_date(update: Update,
+                               context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    await query.delete_message()
+
+    number_of_month_str = query.data
+
+    studio_hl_logger.info(f'Пользователь выбрал месяц: {number_of_month_str}')
+    reserve_user_data = context.user_data['reserve_user_data']
+    schedule_event_ids = reserve_user_data['schedule_event_ids']
+    schedule_events = await db_postgres.get_schedule_events_by_ids(
+        context.session, schedule_event_ids)
+
+    enum_theater_events, schedule_events_filter_by_month = await (
+        get_theater_and_schedule_events_by_month(context,
+                                                 schedule_events,
+                                                 number_of_month_str)
+    )
+
+    text_legend = (
+        '📍 - Премьера\n'
+        '👶🏼 - Рекомендованный возраст\n'
+        '⏳ - Продолжительность\n'
+        '\n'
+    )
+
+    text = '<b>Выберите мероприятие и дату\n</b>' + text_legend
+    text = await create_event_names_text(enum_theater_events, text)
+
+    keyboard = await create_kbd_schedule_and_date(
+        schedule_events_filter_by_month, enum_theater_events)
+    reply_markup = await create_replay_markup(
+        keyboard,
+        postfix_for_cancel=context.user_data['postfix_for_cancel'],
+        postfix_for_back='MONTH',
+        size_row=2
+    )
+
+    await update.effective_chat.send_message(
+        text=text,
+        reply_markup=reply_markup,
+        message_thread_id=update.callback_query.message.message_thread_id,
+    )
+
+    reserve_user_data['number_of_month_str'] = number_of_month_str
+
+    state = 'DATE'
+    set_back_context(context, state, text, reply_markup)
+    context.user_data['STATE'] = state
+    return state
+
+
+async def conversation_timeout(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> int:
+    """Informs the user that the operation has timed out,
+    calls :meth:`remove_reply_markup` and ends the conversation.
+    :return:
+        int: :attr:`telegram.ext.ConversationHandler.END`.
+    """
+    user = context.user_data['user']
+
+    await update.effective_chat.send_message(
+        'От Вас долго не было ответа, пожалуйста выполните новый запрос',
+        message_thread_id=update.effective_message.message_thread_id
+    )
+
+    studio_hl_logger.info(": ".join(
+        [
+            'Пользователь',
+            f'{user}',
+            f'AFK уже {20} мин'
+        ]
+    ))
+
+    return ConversationHandler.END
+
+
+TIMEOUT_HANDLER = TypeHandler(Update, conversation_timeout)
