b85e85f6e5e78af0941a413a3e57cd51eab261e6
MikiEremiki <mr.eremin.ma@ya.ru>
2024-05-27 00:19:22 +0300

    feat: Добавлены файлы migration_admin_conv_hl.py и studio_conv_hl.py
    Добавлены новые файлы python для обработки ситуаций migration и studio. Созданы функции для обработки различных состояний и событий в этих сценариях. Добавлены обработчики для команд, сообщений, обратных запросов и прочего.

---
+++ src/conv_hl/migration_admin_conv_hl.py
@@ -0,0 +1,65 @@
+from typing import Dict, List
+
+from telegram.ext import (
+    BaseHandler,
+    ConversationHandler, CommandHandler, MessageHandler, CallbackQueryHandler,
+    filters,
+)
+
+from handlers import reserve_hl, main_hl, reserve_admin_hl, migration_admin_hl
+from settings.settings import COMMAND_DICT, RESERVE_TIMEOUT
+
+from conv_hl import handlers_event_selection, handlers_client_data_selection
+
+F_text_and_no_command = filters.TEXT & ~filters.COMMAND
+cancel_callback_handler = CallbackQueryHandler(main_hl.cancel,
+                                               pattern='^Отменить')
+back_callback_handler = CallbackQueryHandler(main_hl.back, pattern='^Назад')
+states:  Dict[object, List[BaseHandler]] = {
+    0: [
+        cancel_callback_handler,
+        MessageHandler(F_text_and_no_command,
+                       migration_admin_hl.get_ticket_by_id),
+    ],
+    1: [
+        cancel_callback_handler,
+        back_callback_handler,
+        CallbackQueryHandler(migration_admin_hl.migration_ticket, pattern='migration'),
+        CallbackQueryHandler(migration_admin_hl.update_ticket, pattern='update'),
+        CallbackQueryHandler(reserve_hl.choice_month, pattern='params'),
+        CallbackQueryHandler(reserve_admin_hl.enter_event_id, pattern='id'),
+    ],
+    2: [
+        cancel_callback_handler,
+        back_callback_handler,
+        MessageHandler(F_text_and_no_command,
+                       reserve_admin_hl.choice_option_of_reserve),
+    ],
+    'TICKET': [
+        cancel_callback_handler,
+        back_callback_handler,
+        CallbackQueryHandler(reserve_admin_hl.start_forma_info),
+    ],
+}
+
+for key in handlers_event_selection.keys():
+    states[key] = handlers_event_selection[key]
+for key in handlers_client_data_selection.keys():
+    states[key] = handlers_client_data_selection[key]
+
+for key in states.keys():
+    states[key].append(CommandHandler('reset', main_hl.reset))
+states[ConversationHandler.TIMEOUT] = [reserve_hl.TIMEOUT_HANDLER]
+
+migration_admin_conv_hl = ConversationHandler(
+    entry_points=[
+        CommandHandler(COMMAND_DICT['MIGRATION_ADMIN'][0],
+                       migration_admin_hl.enter_ticket_id),
+    ],
+    states=states,
+    fallbacks=[CommandHandler('help', main_hl.help_command)],
+    conversation_timeout=RESERVE_TIMEOUT * 60,
+    name='migration_admin',
+    persistent=True,
+    allow_reentry=True
+)
---
+++ src/conv_hl/studio_conv_hl.py
@@ -0,0 +1,98 @@
+from typing import Dict, List
+
+from telegram.ext import (
+    BaseHandler,
+    ConversationHandler, CommandHandler, MessageHandler, CallbackQueryHandler,
+    filters,
+)
+from telegram.ext.filters import Text
+
+from conv_hl import handlers_client_data_selection
+from handlers import studio_hl, main_hl, offer_hl, ticket_hl, reserve_hl
+from settings.settings import COMMAND_DICT, RESERVE_TIMEOUT
+
+F_text_and_no_command = filters.TEXT & ~filters.COMMAND
+cancel_callback_handler = CallbackQueryHandler(main_hl.cancel, '^Отменить')
+back_callback_handler = CallbackQueryHandler(main_hl.back, '^Назад')
+states:  Dict[object, List[BaseHandler]] = {
+    'MONTH': [
+        cancel_callback_handler,
+        CallbackQueryHandler(studio_hl.choice_show_and_date),
+    ],
+    'DATE': [
+        cancel_callback_handler,
+        CallbackQueryHandler(main_hl.back, pattern='^Назад-MONTH'),
+        CallbackQueryHandler(main_hl.back, pattern='^Назад-SHOW'),
+        CallbackQueryHandler(reserve_hl.choice_time),
+    ],
+    'TIME': [
+        cancel_callback_handler,
+        CallbackQueryHandler(main_hl.back, pattern='^Назад-DATE'),
+        CallbackQueryHandler(reserve_hl.choice_option_of_reserve),
+    ],
+    'TICKET': [
+        cancel_callback_handler,
+        CallbackQueryHandler(main_hl.back, pattern='^Назад-TIME'),
+        CallbackQueryHandler(ticket_hl.get_ticket),
+    ],
+    'OFFER': [
+        cancel_callback_handler,
+        CallbackQueryHandler(main_hl.back, pattern='^Назад-TICKET'),
+        MessageHandler(Text(('Принимаю',)),
+                       offer_hl.get_agreement),
+    ],
+    'EMAIL': [
+        cancel_callback_handler,
+        CallbackQueryHandler(main_hl.back, pattern='^Назад-TICKET'),
+        CallbackQueryHandler(reserve_hl.get_email, pattern='email_confirm'),
+        MessageHandler(F_text_and_no_command,
+                       reserve_hl.get_email),
+    ],
+    'PAID': [
+        cancel_callback_handler,
+        CallbackQueryHandler(reserve_hl.processing_successful_notification,
+                             pattern='Next'),
+        MessageHandler(
+            filters.PHOTO | filters.ATTACHMENT,
+            reserve_hl.forward_photo_or_file
+        ),
+    ],
+    'LIST': [
+        cancel_callback_handler,
+        back_callback_handler,
+        CallbackQueryHandler(reserve_hl.send_clients_data),
+    ],
+    'CHOOSING': [
+        MessageHandler(
+            filters.Regex('^(Выбрать другое время)$'),
+            reserve_hl.choice_month
+        ),
+        MessageHandler(
+            filters.Regex('^(Записаться в лист ожидания)$'),
+            reserve_hl.write_list_of_waiting
+        ),
+    ],
+    'PHONE_FOR_WAITING': [
+        MessageHandler(F_text_and_no_command,
+                       reserve_hl.get_phone_for_waiting),
+    ],
+}
+
+for key in handlers_client_data_selection.keys():
+    states[key] = handlers_client_data_selection[key]
+
+for key in states.keys():
+    states[key].append(CommandHandler('reset', main_hl.reset))
+states[ConversationHandler.TIMEOUT] = [studio_hl.TIMEOUT_HANDLER]
+
+studio_conv_hl = ConversationHandler(
+    entry_points=[
+        CommandHandler(COMMAND_DICT['STUDIO'][0], reserve_hl.choice_month),
+    ],
+    states=states,
+    fallbacks=[CommandHandler('help', main_hl.help_command)],
+    conversation_timeout=RESERVE_TIMEOUT * 60,
+    name='studio',
+    persistent=True,
+    allow_reentry=True
+)
