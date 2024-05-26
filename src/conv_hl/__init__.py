9ee61646b261123e8258dd8134014a0af11fb31d
MikiEremiki <mr.eremin.ma@ya.ru>
2024-05-27 00:12:46 +0300

    refactor: Реформатирование импорта и добавление новых обработчиков в __init__.py
    Обновлен формат импорта в __init__.py для повышения читаемости. Были также добавлены обработчики для migration_admin_conv_hl и studio_conv_hl.

--- src/conv_hl/__init__.py
+++ src/conv_hl/__init__.py
@@ -1,5 +1,10 @@
-from .common_hl import (base_handlers, F_text_and_no_command,
-                        cancel_callback_handler, back_callback_handler)
+from .common_hl import (
+    handlers_event_selection,
+    handlers_client_data_selection,
+    F_text_and_no_command,
+    cancel_callback_handler,
+    back_callback_handler
+)
 
 from .reserve_conv_hl import reserve_conv_hl
 from .reserve_admin_conv_hl import reserve_admin_conv_hl
@@ -7,3 +12,5 @@ from .list_wait_conv_hl import list_wait_conv_hl
 from .birthday_conv_hl import birthday_conv_hl, birthday_paid_conv_hl
 from .afisha_conv_hl import afisha_conv_hl
 from .support_conv_hl import support_conv_hl
+from .migration_admin_conv_hl import migration_admin_conv_hl
+from .studio_conv_hl import studio_conv_hl
