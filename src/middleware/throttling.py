49f97180b2ee563d75fefbb5b7aa5d372a184eb7
MikiEremiki <mr.eremin.ma@ya.ru>
2024-10-20 15:11:41 +0300

    feat: Расширение функциональности throttling middleware
    - Улучшение читаемости логов
    - Добавление проверки callback_data на повторную обработку

--- src/middleware/throttling.py
+++ src/middleware/throttling.py
@@ -8,12 +8,25 @@ logger_ttl = logging.getLogger('bot.TTL')
 CACHE = TTLCache(maxsize=10_000, ttl=0.3)
 
 def add_throttling_middleware(application):
-    async def check_ttl(update: Update, _: ContextTypes.DEFAULT_TYPE):
+    async def check_ttl(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if update.effective_user.id in CACHE:
-            logger_ttl.info(
-                f'User: {update.effective_user}: часто нажимает на '
-                f'кнопку')
+            logger_ttl.warning(
+                f'От пользователя приходит слишком много update: {update}')
             raise ApplicationHandlerStop
+
+        query = update.callback_query
+        if query:
+            last_data = context.user_data.get('last_callback_query', None)
+            if last_data == query.data:
+                logger_ttl.warning(
+                    f'Update уже обработан: callback_data: {query.data}')
+
+                raise ApplicationHandlerStop
+            else:
+                context.user_data['last_callback_query'] = query.data
+                logger_ttl.info(
+                    f'save last_callback_data: {query.data}: '
+                    f'{update.effective_user}')
 
         CACHE[update.effective_user.id] = True
 
