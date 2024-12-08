f3748d3da118b30cb864701a189c4d5f3c4e3a7a
MikiEremiki <mr.eremin.ma@ya.ru>
2024-12-09 01:25:05 +0300

    feat: добавлен фильтр для отправки сообщений
    Добавлен новый фильтр для проверки пользователей, которым разрешено отправлять сообщения. Это улучшает контроль доступа и безопасность при отправке сообщений.

--- src/custom_filters/admin.py
+++ src/custom_filters/admin.py
@@ -1,5 +1,7 @@
 from telegram.ext import filters
 
 from settings.settings import ADMIN_ID
+from settings.settings import CHAT_ID_KOCHETKOVA
 
 filter_admin = filters.User(ADMIN_ID)
+filter_to_send_msg = filters.User(ADMIN_ID + [CHAT_ID_KOCHETKOVA])
--- src/handlers/set_handlers.py
+++ src/handlers/set_handlers.py
@@ -69,6 +69,7 @@ def set_handlers(application, config):
                              pattern='Next'))
 
     filter_admin = custom_filters.filter_admin
+    filter_to_send_msg = custom_filters.filter_to_send_msg
     application.add_handlers([
         CommandHandler('clean_ud', clean_ud, filter_admin),
         CommandHandler('print_ud', print_ud, filter_admin),
@@ -82,7 +83,7 @@ def set_handlers(application, config):
                        filter_admin),
         CommandHandler('send_msg',
                        main_hl.send_msg,
-                       filter_admin),
+                       filter_to_send_msg),
         CommandHandler(COMMAND_DICT['LOG'][0], send_log, filter_admin),
         CommandHandler("postgres_log", send_postgres_log, filter_admin),
         CommandHandler(COMMAND_DICT['CB_TW'][0], get_balance, filter_admin),
