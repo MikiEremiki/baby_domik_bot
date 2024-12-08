e46414d63e7c9ea7fe017a43f312af82e1814e42
MikiEremiki <mr.eremin.ma@ya.ru>
2024-12-09 01:37:49 +0300

    refactor: улучшение обработки статусов платежей
    Код разделен на функции для обработки успешной оплаты билетов и предоплаты заказных мероприятий.
    Обработаны возможные ошибки при отправке сообщений, добавлены логи.

--- src/handlers/webhook_hl.py
+++ src/handlers/webhook_hl.py
@@ -9,7 +9,7 @@ from api.googlesheets import update_ticket_in_gspread
 from db import db_postgres
 from db.enum import TicketStatus
 from handlers.sub_hl import send_approve_reject_message_to_admin_in_webhook
-from settings.settings import CHAT_ID_MIKIEREMIKI
+from settings.settings import CHAT_ID_MIKIEREMIKI, ADMIN_GROUP
 
 webhook_hl_logger = logging.getLogger('bot.webhook')
 
@@ -32,75 +32,118 @@ async def webhook_update(update: WebhookNotification,
     if update.object.status == 'pending':
         await context.bot.send_message(CHAT_ID_MIKIEREMIKI,
                                        'Платеж ожидает оплаты')
-    if update.object.status == 'waiting_for_capture':
+    elif update.object.status == 'waiting_for_capture':
         pass  # Для двух стадийной оплаты
-    if update.object.status == 'succeeded':
-        message_id = update.object.metadata['message_id']
-        chat_id = update.object.metadata['chat_id']
-        ticket_ids = update.object.metadata['ticket_ids'].split('|')
-        ticket_ids = [int(ticket_id) for ticket_id in ticket_ids]
-        choose_schedule_event_ids = update.object.metadata[
-            'choose_schedule_event_ids'].split('|')
-        command = update.object.metadata['command']
+    elif update.object.status == 'succeeded':
+        prepaid_invoice_number = '305862-32'
+        invoice_original_number = update.object.metadata.get(
+            'dashboardInvoiceOriginalNumber', False)
+        if invoice_original_number == prepaid_invoice_number:
+           await processing_cme_prepaid(update, context)
+        elif update.object.metadata.get('command', False):
+            await processing_ticket_paid(update, context)
+    elif update.object.status == 'canceled':
+        pass
 
-        webhook_hl_logger.info(
-            f'webhook_update: '
-            f'{ticket_ids=}, '
-            f'{choose_schedule_event_ids=}, '
-        )
 
+async def processing_ticket_paid(update, context):
+    message_id = update.object.metadata['message_id']
+    chat_id = update.object.metadata['chat_id']
+    ticket_ids = update.object.metadata['ticket_ids'].split('|')
+    ticket_ids = [int(ticket_id) for ticket_id in ticket_ids]
+    choose_schedule_event_ids = update.object.metadata[
+        'choose_schedule_event_ids'].split('|')
+    command = update.object.metadata['command']
+    webhook_hl_logger.info(
+        f'webhook_update: '
+        f'{ticket_ids=}, '
+        f'{choose_schedule_event_ids=}, '
+    )
+    try:
         await context.bot.edit_message_reply_markup(chat_id, message_id)
-
-        text = f'<b>Номер вашего билета '
-        ticket_status = TicketStatus.PAID
-        for ticket_id in ticket_ids:
-            update_ticket_in_gspread(ticket_id, ticket_status.value)
-            await db_postgres.update_ticket(context.session,
-                                            ticket_id,
-                                            status=ticket_status)
-            text += f'<code>{ticket_id}</code> '
-        text += '</b>\n\n'
-
-        text += (
-            'Платеж успешно обработан\n\n'
-            'Нажмите <b>«ДАЛЕЕ»</b> под сообщением для получения более '
-            'подробной информации\n\n'
-            '<i>Или отправьте квитанцию/чек об оплате</i>')
-        reply_markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton('ДАЛЕЕ', callback_data='Next')]])
+    except BadRequest as e:
+        webhook_hl_logger.error(e)
+        webhook_hl_logger.error('Удаление кнопки для оплаты не произошло')
+    text = f'<b>Номер вашего билета '
+    ticket_status = TicketStatus.PAID
+    for ticket_id in ticket_ids:
+        update_ticket_in_gspread(ticket_id, ticket_status.value)
+        await db_postgres.update_ticket(context.session,
+                                        ticket_id,
+                                        status=ticket_status)
+        text += f'<code>{ticket_id}</code> '
+    text += '</b>\n\n'
+    text += (
+        'Платеж успешно обработан\n\n'
+        'Нажмите <b>«ДАЛЕЕ»</b> под сообщением для получения более '
+        'подробной информации\n\n'
+        '<i>Или отправьте квитанцию/чек об оплате</i>')
+    reply_markup = InlineKeyboardMarkup(
+        [[InlineKeyboardButton('ДАЛЕЕ', callback_data='Next')]])
+    try:
         await context.bot.send_message(chat_id=chat_id,
                                        text=text,
                                        reply_markup=reply_markup)
-
-        thread_id = None
-        callback_name = None
-        if command == 'reserve':
+    except BadRequest as e:
+        webhook_hl_logger.error(e)
+        webhook_hl_logger.error(
+            'Отправка сообщения о обработке платежа не произошла')
+    thread_id = None
+    callback_name = None
+    if command == 'reserve':
+        thread_id = (context.bot_data['dict_topics_name']
+                     .get('Бронирования спектаклей', None))
+        # TODO Переписать ключи словаря с топиками на использование enum
+        if not thread_id:
             thread_id = (context.bot_data['dict_topics_name']
-                         .get('Бронирования спектаклей', None))
-            # TODO Переписать ключи словаря с топиками на использование enum
-            if not thread_id:
-                thread_id = (context.bot_data['dict_topics_name']
-                             .get('Бронирование спектаклей', None))
-            callback_name = 'reserve'
-        if command == 'studio':
-            thread_id = (context.bot_data['dict_topics_name']
-                         .get('Бронирования студия', None))
-            callback_name = 'studio'
-        try:
-            await send_approve_reject_message_to_admin_in_webhook(context,
-                                                                  chat_id,
-                                                                  message_id,
-                                                                  ticket_ids,
-                                                                  thread_id,
-                                                                  callback_name)
-        except NameError as e:
-            webhook_hl_logger.error(e)
-            await context.bot.send_message(CHAT_ID_MIKIEREMIKI, f'{command=}')
+                         .get('Бронирование спектаклей', None))
+        callback_name = 'reserve'
+    if command == 'studio':
+        thread_id = (context.bot_data['dict_topics_name']
+                     .get('Бронирования студия', None))
+        callback_name = 'studio'
+    try:
+        await send_approve_reject_message_to_admin_in_webhook(context,
+                                                              chat_id,
+                                                              message_id,
+                                                              ticket_ids,
+                                                              thread_id,
+                                                              callback_name)
+    except (NameError, BadRequest) as e:
+        webhook_hl_logger.error(e)
+        error_text = 'Отправление в админский чат не произошло'
+        webhook_hl_logger.error(error_text)
+        text = f'{error_text} {ticket_ids=} {command=}'
+        await context.bot.send_message(CHAT_ID_MIKIEREMIKI, text)
 
-        state = 'PAID'
-        return state
-    if update.object.status == 'canceled':
-        pass
+
+async def processing_cme_prepaid(update, context):
+    thread_id = (context.bot_data['dict_topics_name']
+                 .get('Выездные мероприятия', None))
+    try:
+        text = f'#Предоплата\n'
+        text += f'Платеж успешно обработан\n'
+        customer_email = update.object.metadata['custEmail']
+        customer_number = update.object.metadata['customerNumber']
+        invoice_original_number = update.object.metadata['dashboardInvoiceOriginalNumber']
+        text += f'Покупатель: {customer_email}'
+        text += f'Номер счета: {invoice_original_number}'
+        if customer_number != customer_email:
+            text += f' {customer_number}'
+
+        text += 'Ждем сообщения от пользователя с квитанцией и номером заявки'
+
+        await context.bot.send_message(
+            chat_id=ADMIN_GROUP,
+            text=text,
+            message_thread_id=thread_id,
+        )
+    except (NameError, BadRequest) as e:
+        webhook_hl_logger.error(e)
+        error_text = 'Отправление в админский чат не произошло'
+        webhook_hl_logger.error(error_text)
+        text = f'{error_text}'
+        await context.bot.send_message(CHAT_ID_MIKIEREMIKI, text)
 
 
 WebhookHandler = TypeHandler(WebhookNotification, webhook_update)
