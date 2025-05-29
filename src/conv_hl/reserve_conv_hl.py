d63bb61d925ffac90dae22724125b8bf1cf13e38
MikiEremiki <mr.eremin.ma@ya.ru>
2025-05-30 01:14:05 +0300

    feat: добавить фильтр для команды списка в reserve_conv_hl
    Добавлен новый фильтр `filter_list_cmd` в `custom_filters.admin` для проверки доступа к команде списка в `reserve_conv_hl`. Это позволяет ограничить использование команды определенной группой пользователей.

--- src/conv_hl/reserve_conv_hl.py
+++ src/conv_hl/reserve_conv_hl.py
@@ -4,6 +4,7 @@ from telegram.ext import (
 )
 from telegram.ext.filters import Text
 
+from custom_filters.admin import filter_list_cmd
 from handlers import reserve_hl, main_hl, offer_hl, ticket_hl
 from conv_hl import (
     handlers_event_selection, handlers_client_data_selection,
@@ -71,7 +72,9 @@ states[ConversationHandler.TIMEOUT] = [reserve_hl.TIMEOUT_HANDLER]
 reserve_conv_hl = ConversationHandler(
     entry_points=[
         CommandHandler(COMMAND_DICT['RESERVE'][0], reserve_hl.choice_month),
-        CommandHandler(COMMAND_DICT['LIST'][0], reserve_hl.choice_month),
+        CommandHandler(COMMAND_DICT['LIST'][0],
+                       reserve_hl.choice_month,
+                       filter_list_cmd),
     ],
     states=states,
     fallbacks=common_fallbacks,
--- src/custom_filters/admin.py
+++ src/custom_filters/admin.py
@@ -1,7 +1,8 @@
 from telegram.ext import filters
 
-from settings.settings import ADMIN_ID
+from settings.settings import ADMIN_ID, CHAT_ID_MIKLERES
 from settings.settings import CHAT_ID_KOCHETKOVA
 
 filter_admin = filters.User(ADMIN_ID)
 filter_to_send_msg = filters.User(ADMIN_ID + [CHAT_ID_KOCHETKOVA])
+filter_list_cmd = filters.User(ADMIN_ID + [CHAT_ID_KOCHETKOVA])
