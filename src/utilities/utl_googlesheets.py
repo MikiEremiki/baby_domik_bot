8bee429fdee1427f96878a73f0f7d10320fcfb44
MikiEremiki <mr.eremin.ma@ya.ru>
2025-07-03 10:44:48 +0300

    fix: исправление обработки отсутствующего билета в базе

--- src/utilities/utl_googlesheets.py
+++ src/utilities/utl_googlesheets.py
@@ -20,38 +20,37 @@ async def write_to_return_seats_for_sale(context):
         for ticket_id in ticket_ids:
             ticket = await db_postgres.get_ticket(
                 context.session, ticket_id)
-            text = f'Билету|{ticket.id}-{ticket.status.value}|'
             if ticket is None:
-                text += 'Билет отсутствует в базе'
-                utl_googlesheets_logger.warning(text)
-            elif ticket.status == TicketStatus.CREATED and changed_seat:
-                schedule_event_id = ticket.schedule_event_id
-                base_ticket_id = ticket.base_ticket_id
-                if '_admin' in command:
-                    result = await increase_free_seat(
-                        context, schedule_event_id, base_ticket_id)
-                    text += f'increase_free|{schedule_event_id=}'
-                else:
-                    result = await increase_free_and_decrease_nonconfirm_seat(
-                        context, schedule_event_id, base_ticket_id)
-                    text += f'increase_free_and_decrease_nonconfirm|{schedule_event_id=}'
-                if not result:
-                    text += '|Надо проверить и возможно отменить билет в ручную'
-                    await context.bot.send_message(
-                        chat_id=context.config.bot.developer_chat_id,
-                        text=text)
-                else:
-                    await update_ticket_db_and_gspread(
-                        context, ticket_id, status=ticket_status)
+                text = f'Билет {ticket_id} отсутствует в базе'
             else:
-                text += 'Нельзя отменять'
-            utl_googlesheets_logger.warning(text)
+                text = f'Билет|{ticket.id}-{ticket.status.value}|'
+                if ticket.status == TicketStatus.CREATED and changed_seat:
+                    schedule_event_id = ticket.schedule_event_id
+                    base_ticket_id = ticket.base_ticket_id
+                    if '_admin' in command:
+                        result = await increase_free_seat(
+                            context, schedule_event_id, base_ticket_id)
+                        text += f'increase_free|{schedule_event_id=}'
+                    else:
+                        result = await increase_free_and_decrease_nonconfirm_seat(
+                            context, schedule_event_id, base_ticket_id)
+                        text += f'increase_free_and_decrease_nonconfirm|{schedule_event_id=}'
+                    if not result:
+                        text += '|Надо проверить и возможно отменить билет в ручную'
+                        await context.bot.send_message(
+                            chat_id=context.config.bot.developer_chat_id,
+                            text=text)
+                    else:
+                        await update_ticket_db_and_gspread(
+                            context, ticket_id, status=ticket_status)
+                else:
+                    text += 'Нельзя отменять'
             await context.bot.send_message(
                 chat_id=context.config.bot.developer_chat_id,
                 text=text)
     else:
         text = 'Билетов для отмены нет'
-        utl_googlesheets_logger.warning(text)
+    utl_googlesheets_logger.warning(text)
 
 
 async def update_ticket_db_and_gspread(context, ticket_id, **kwargs):
