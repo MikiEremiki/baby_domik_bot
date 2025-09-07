6696c95af0a8cc403aac2504f8d1437855e5db34
MikiEremiki <mr.eremin.ma@ya.ru>
2025-09-07 16:18:26 +0300

    refactor: Добавление типов

--- src/utilities/utl_db.py
+++ src/utilities/utl_db.py
@@ -1,7 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from db import create_sessionmaker_and_engine
 
 
-async def open_session(config):
+async def open_session(config) -> AsyncSession:
     sessionmaker = create_sessionmaker_and_engine(str(config.postgres.db_url))
 
     session = sessionmaker()
