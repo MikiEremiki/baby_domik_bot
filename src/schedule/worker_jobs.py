c91ea149944c5f0b9e87347d0241aba64fc75034
MikiEremiki <mr.eremin.ma@ya.ru>
2025-09-07 16:29:14 +0300

    refactor: Добавление обработки ошибок в задании по отправке напоминаний

--- src/schedule/worker_jobs.py
+++ src/schedule/worker_jobs.py
@@ -1,7 +1,10 @@
+import asyncio
 import logging
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from typing import List
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from telegram.error import RetryAfter
 from telegram.ext import ContextTypes
 
 from db import db_postgres, Ticket
@@ -27,7 +30,8 @@ async def send_reminder(context: "ContextTypes.DEFAULT_TYPE") -> None:
     full_name = get_full_name_event(theater)
 
     for ticket in tickets:
-        if ticket.status in [TicketStatus.PAID, TicketStatus.APPROVED] and not getattr(ticket, 'reminded_1d_at', None):
+        if (ticket.status in [TicketStatus.PAID, TicketStatus.APPROVED]
+                and not getattr(ticket, 'reminded_1d_at', None)):
             text = (f"Напоминаем о предстоящем мероприятии завтра!\n\n"
                     f'<b>{full_name}\n'
                     f'{date_event}\n'
@@ -39,15 +43,34 @@ async def send_reminder(context: "ContextTypes.DEFAULT_TYPE") -> None:
             text += ('Задать вопросы можно в сообщениях группы\n'
                      'https://vk.com/baby_theater_domik')
             try:
-                await context.bot.send_message(
-                    chat_id=ticket.user.chat_id,
-                    text=text
-                )
-                ticket.reminded_1d_at = datetime.now(timezone.utc)
-                await session.commit()
+                await send_message(context, session, text, ticket)
+            except RetryAfter as e:
+                worker_logger.error(
+                    f"RetryAfter for ticket {ticket.id}: {e}")
+                delay = e.retry_after
+                if isinstance(e.retry_after, timedelta):
+                    delay = e.retry_after.total_seconds()
+                await asyncio.sleep(delay)
+                await send_message(context, session, text, ticket)
             except Exception as e:
                 worker_logger.error(
                     f"Failed to send reminder for ticket {ticket.id}: {e}")
+                worker_logger.info('Попытка повтора')
+                await send_message(context, session, text, ticket)
         else:
             worker_logger.info(f"Ticket {ticket.id} already reminded")
     await session.close()
+
+
+async def send_message(
+        context: "ContextTypes.DEFAULT_TYPE",
+        session: AsyncSession,
+        text: str,
+        ticket: Ticket
+):
+    await context.bot.send_message(
+        chat_id=ticket.user.chat_id,
+        text=text
+    )
+    ticket.reminded_1d_at = datetime.now(timezone.utc)
+    await session.commit()
