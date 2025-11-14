4fae41a1fbb3d0ace0f6a74209df94bdc7b49e82
MikiEremiki <mr.eremin.ma@ya.ru>
2025-11-15 00:00:22 +0300

    feat: Выделение новогодних спектаклей в отдельную группу для выбора в режиме по репертуару

--- src/conv_hl/list_wait_conv_hl.py
+++ src/conv_hl/list_wait_conv_hl.py
@@ -2,7 +2,7 @@ from telegram.ext import (
     ConversationHandler, CommandHandler, CallbackQueryHandler,
 )
 
-from custom_filters import filter_admin
+from custom_filters import filter_list_cmd
 from handlers import reserve_hl, main_hl, list_wait_hl
 from conv_hl import cancel_callback_handler, common_fallbacks
 from settings.settings import COMMAND_DICT, RESERVE_TIMEOUT
@@ -33,7 +33,7 @@ list_wait_conv_hl = ConversationHandler(
     entry_points=[
         CommandHandler(COMMAND_DICT['LIST_WAIT'][0],
                        reserve_hl.choice_month,
-                       filter_admin),
+                       filter_list_cmd),
     ],
     states=states,
     fallbacks=common_fallbacks,
