96aeee25348cc38ad5111b53aec0d7eead945207
MikiEremiki <mr.eremin.ma@ya.ru>
2026-02-16 06:43:20 +0300

    feat: добавлен middleware для проверки данных резервирования
    Добавлен новый middleware `add_reserve_check_middleware` для управления обработкой данных резервирования. Обновлен `__all__`, чтобы включить новый middleware.

--- src/middleware/__init__.py
+++ src/middleware/__init__.py
@@ -2,3 +2,13 @@ from .db import add_db_handlers_middleware
 from .glob_usage_users import add_glob_on_off_middleware
 from .tg_update_logging import add_tg_update_logging_middleware
 from .user_status import add_user_status_middleware
+from .reserve_check import add_reserve_check_middleware
+
+
+__all__ = [
+    'add_db_handlers_middleware',
+    'add_glob_on_off_middleware',
+    'add_tg_update_logging_middleware',
+    'add_user_status_middleware',
+    'add_reserve_check_middleware',
+]
